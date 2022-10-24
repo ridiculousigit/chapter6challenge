6234a17f0e8517affaf490ade27f935c296fee88
ridiculousigit <jsigit243@gmail.com>
2022-10-24 13:38:23 +0700

    Initial commit

---
+++ app/src/main/java/binar/academy/chapter6challenge/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package binar.academy.chapter6challenge.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import binar.academy.chapter6challenge.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
